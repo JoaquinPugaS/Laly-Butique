ae2338dff3e6b0b3edd41c55adb7a1db60397976
devLucasSsS <107324428+devLucasSsS@users.noreply.github.com>
2022-09-28 19:41:54 -0300

    updates

--- lalybutique/src/App.vue
+++ lalybutique/src/App.vue
@@ -2,16 +2,13 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <router-link to="/Editar">Editar</router-link>
-    <router-link to="/Crear">Crear</router-link>
-    <router-link to="/Listar">Listar</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
 #app {
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
