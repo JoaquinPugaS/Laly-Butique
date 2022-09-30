df6b82992c7b07ef1d5f17dda2e39744b91a483d
devLucasSsS <107324428+devLucasSsS@users.noreply.github.com>
2022-09-30 15:47:54 -0300

    Update CloudinaryUpload.vue

--- lalybutique/src/components/CloudinaryUpload.vue
+++ lalybutique/src/components/CloudinaryUpload.vue
@@ -1,3 +1,11 @@
 <script>
 import axios from "axios";
+import VueProgressBar from 'vue-progressbar';
+
+export default{
+    name: 'CloudinaryUpload',
+    components:{
+        VueProgressBar
+    },
+}
 </script>
